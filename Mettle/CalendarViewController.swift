ecaf01d27bfa4c585f77a22c75a663ee2c2265ac
Matthew Linkous <matthew.linkous@gmail.com>
2017-04-26 23:34:21 -0400

    Added basic calendar functionality to calendar tab

---
+++ Mettle/CalendarViewController.swift
@@ -0,0 +1,62 @@
+//
+//  CalendarViewController.swift
+//  Mettle
+//
+//  Created by Matthew Linkous on 4/26/17.
+//  Copyright © 2017 Peter Huffman. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController {
+    let formatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.DateLabel.text = cellState.text
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? CustomCell else { return }
+        validCell.selectedView.isHidden = false
+    }
+}
---
+++ Mettle/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  Mettle
+//
+//  Created by Matthew Linkous on 4/26/17.
+//  Copyright © 2017 Peter Huffman. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var DateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    
+}
