c45ec9a1c0818d4762919cd3c420bf1a260b87b1
Matthew Linkous <matthew.linkous@gmail.com>
2017-04-27 14:17:05 -0400

    Merged settings, calendar, logs

---
+++ Mettle/SwitchCell.swift
@@ -0,0 +1,26 @@
+//
+//  SwitchCell.swift
+//  Mettle
+//
+//  Created by Gordon Nickerson on 4/26/17.
+//  Copyright © 2017 Peter Huffman. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchElmt: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
